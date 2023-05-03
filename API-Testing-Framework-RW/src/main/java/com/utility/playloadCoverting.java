6be2a4c1d19c80ff416479a4a7c6ddba66d5e864
Sukhmani-Kaur1 <sukhmani.dev006@gmail.com>
2023-05-03 12:30:15 +0530

    Test files added

--- API-Testing-Framework-RW/src/main/java/com/utility/playloadCoverting.java
+++ API-Testing-Framework-RW/src/main/java/com/utility/playloadCoverting.java
@@ -8,7 +8,7 @@ import java.nio.file.Paths;
 public class playloadCoverting {
  public static String generatingLoadString(String filename) throws IOException {
 //	String filePath = "/Users/Administrator/Documents/workspace-spring-tool-suite-4-4.17.2.RELEASE/APITesting-Cucumber/src/test/resources/"+filename;
-	String filePath = System.getProperty("user.dir")+"\\src\\main\\resources\\"+filename;
+	String filePath = System.getProperty("user.dir")+"\\src\\test\\resources\\"+filename;
 	return new String(Files.readAllBytes(Paths.get(filePath)));
 	
 			
