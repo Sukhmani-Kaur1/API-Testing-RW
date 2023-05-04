417d1c252fe080cf16a82f15b9a94a98026de01c
Jaydip97 <pjaydip460@gmail.com>
2023-05-04 12:29:41 +0530

    latest code jaydip finish

--- API-Testing-Framework-RW/src/main/java/com/utility/Auth.java
+++ API-Testing-Framework-RW/src/main/java/com/utility/Auth.java
@@ -2,7 +2,7 @@ package com.utility;
 
 public class Auth {
 	public final static String OWNER = "Jaydip97";
- public final static String BEARER_TOKEN_STRING = "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";
+ public final static String BEARER_TOKEN_STRING = "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";
  public static String getToken() {
 	return BEARER_TOKEN_STRING;
 }
