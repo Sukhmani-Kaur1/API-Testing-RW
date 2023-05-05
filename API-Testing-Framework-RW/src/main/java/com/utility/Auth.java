cb5ce5c7ba7f120c45df8635c2a7a4a21625f456
Sukhmani-Kaur1 <sukhmani.dev006@gmail.com>
2023-05-05 10:39:27 +0530

    final submit

--- API-Testing-Framework-RW/src/main/java/com/utility/Auth.java
+++ API-Testing-Framework-RW/src/main/java/com/utility/Auth.java
@@ -2,7 +2,7 @@ package com.utility;
 
 public class Auth {
 	public final static String OWNER = "Sukhmani-Kaur1";
- public final static String BEARER_TOKEN_STRING = "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";
+ public final static String BEARER_TOKEN_STRING = "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";
  public static String getToken() {
 	return BEARER_TOKEN_STRING;
 }
