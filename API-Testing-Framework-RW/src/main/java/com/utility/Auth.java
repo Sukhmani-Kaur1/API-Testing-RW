f60572276c53aa8a3f7d138b1080b405975f0b7e
Sukhmani kaur <101392918+Sukhmani-Kaur1@users.noreply.github.com>
2023-05-05 13:12:50 +0530

    Delete Auth.java

--- API-Testing-Framework-RW/src/main/java/com/utility/Auth.java
+++
@@ -1,9 +0,0 @@
-package com.utility;
-
-public class Auth {
-	public final static String OWNER = "1803amar";
- public final static String BEARER_TOKEN_STRING = "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";
- public static String getToken() {
-	return BEARER_TOKEN_STRING;
-}
-}
